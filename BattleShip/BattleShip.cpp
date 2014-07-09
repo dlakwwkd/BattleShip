56f88a1da121f19af5090e24136325507ae3aa8e
dlakwwkd <dlakek@naver.com>
2014-07-09 20:23:01 +0900

    2014.07.09

--- BattleShip/BattleShip.cpp
+++ BattleShip/BattleShip.cpp
@@ -3,9 +3,12 @@
 
 #include "stdafx.h"
 
-
 int _tmain(int argc, _TCHAR* argv[])
 {
+	//UNREFERENCED_PARAMETER(argc);
+	argc;
+	argv;
+
 	return 0;
 }
 
--- BattleShip_pp/BattleShip_pp.cpp
+++ BattleShip_pp/BattleShip_pp.cpp
@@ -2,10 +2,36 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <string>
+#include <time.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	//UNREFERENCED_PARAMETER(argc);
+	argc;
+	argv;
+
+	std::string input_str;
+	std::string guess_request = "ÁÂÇ¥ ÀÔ·Â: ";
+	std::string respone[] = { "HIT", "MISS", "DESTROY" };
+	
+	srand(time(NULL));
+
+	while (1)
+	{
+		std::cout << guess_request;
+		std::cin >> input_str;
+
+		if (input_str == "quit") break;
+
+		int random = rand() % 3;
+
+		std::cout << respone[random] << std::endl;
+
+	}
+
+	
 	return 0;
 }
 
