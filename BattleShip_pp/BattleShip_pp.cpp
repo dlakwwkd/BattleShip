43b2a7837b951ebcc85266bc981662f6c45e7e60
dlakwwkd <dlakek@naver.com>
2014-07-16 22:26:17 +0900

    2주차

--- BattleShip_pp/BattleShip_pp.cpp
+++ BattleShip_pp/BattleShip_pp.cpp
@@ -5,33 +5,66 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <vector>
+#include <windows.h>
+
+std::vector<std::string> units;
+std::vector<std::string> checkUnits;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	//UNREFERENCED_PARAMETER(argc);
 	argc;
 	argv;
+	units.push_back("Aircraft");
+	units.push_back("Battleship");
+	units.push_back("Battleship");
+	units.push_back("Cruiser");
+	units.push_back("Destroyer");
+	units.push_back("Destroyer");
 
 	std::string input_str;
 	std::string guess_request = "ÁÂÇ¥ ÀÔ·Â: ";
 	std::string respone[] = { "HIT", "MISS", "DESTROY" };
-	
+
 	srand(time(NULL));
 
-	while (1)
+	while (!(units.empty()))
 	{
+		system("cls");
+		for (int i = 0; i < units.size(); i++)
+		{
+			std::cout << units[i] << "   ";
+		}
+		std::cout << std::endl;
+
 		std::cout << guess_request;
 		std::cin >> input_str;
 
 		if (input_str == "quit") break;
+		int random = rand() % _countof(respone);
+		int randPick = rand() % units.size();
 
-		int random = rand() % 3;
-
-		std::cout << respone[random] << std::endl;
-
+		switch (random)
+		{
+		case 0:
+			std::cout << respone[random] << std::endl;
+			break;
+		case 1:
+			std::cout << respone[random] << std::endl;
+			break; 
+		case 2:
+			std::cout << units[randPick] << " is DESTROYED!" << std::endl;
+			units.erase(units.begin() + randPick);
+			break;
+		}
+		std::cout << "\nNext Turn!" << std::endl;
+		getchar();
+		getchar();
 	}
+	std::cout << "You win!" << std::endl;
+	getchar();
 
-	
 	return 0;
 }
 
